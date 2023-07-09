2a7f9dcb4c9c2af3c59877d10e1d8f4d4a9ef430
anchan828 <anchan828@gmail.com>
2023-07-09 14:54:18 +0900

    chore: update lint-stage

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  "*.{js,ts}": ["eslint --fix", "git add"],
+  "*.{js,ts}": ["eslint --fix", "prettier --write", "git add"],
   "*.{json,yml,yaml,md,graphql}": ["prettier --write", "git add"],
 };
